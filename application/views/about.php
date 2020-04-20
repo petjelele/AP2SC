f4dc2be19e243da4dfb4a7bcd49e07e0e626fc4c
petjelele <63584610+petjelele@users.noreply.github.com>
2020-04-20 10:19:03 +0700

    Add files via upload

---
+++ application/views/about.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+        <section id="home" class="home">
+            <!-- Carousel -->
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <!-- Carousel-inner -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="assets/frontend/images/slider.jpg" alt="Construction">
+                        <div class="overlay">
+                            <div class="carousel-caption">
+                                <h3>Aplikasi Pengembangan dan Pelatihan Service Center</h3>
+                                <h1>AP2SC</h1>
+                                <h1 class="second_heading">UNIKOM</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="assets/frontend/images/slider_2.jpg" alt="Construction">
+                        <div class="overlay">
+                            <div class="carousel-caption">
+                                <h3>Aplikasi Pengembangan dan Pelatihan Service Center</h3>
+                                <h1>AP2SC</h1>
+                                <h1 class="second_heading">UNIKOM</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>  
+                <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+                    <span class="fa fa-angle-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+                    <span class="fa fa-angle-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+        </section>
+
+
+        <!-- About -->
+        <section id="about">
+            <div class="container about_bg">
+                <div class="row">
+                    <div class="col-lg-7 col-md-6">
+                        <div class="about_content">
+                            <h2>Selamat Datang Di AP2SC</h2>
+                            <h3>Aplikasi Pengembangan dan Pelatihan Service Center</h3>
+                            <p>,</p>
+                            <p> </p>
+                            <div class="row">
+                              <div class="col">
+                                    IT Training & Service Center (ITSC) UNIKOM merupakan salah satu produk unggulan dari IPTEK bagi inovasi dan kreatifitas kampus UNIKOM, yang berdiri sejak tahun 2008 sampai saat ini. ITSC UNIKOM merupakan lembaga yang bergerak di di bidang jasa pelayanan training/kursus bidang teknologi informasi dan multimedia, dan juga merupakan penyedia layanan jasa service center seperti smartphone dan laptop.
+
+                                    Selain itu, ITSC juga merupakan penyedia jasa untuk pembuatan Aplikasi dan Multimedia. Trainer yang berada di ITSC juga merupakan Trainer yang berpengalaman serta memiliki sertifikat kompetensi.
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-lg-offset-1">
+                        <div class="about_banner">
+                            <img src="assets/frontend/images/Logo AP2SC.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- About end -->
