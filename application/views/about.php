fcfac9b924fe9ca40da1ec876e7260a359e9a3db
petjelele <63584610+petjelele@users.noreply.github.com>
2020-08-10 21:51:14 +0700

    Add files via upload

--- application/views/about.php
+++ application/views/about.php
@@ -10,12 +10,19 @@
                     </div>
                     <div class="col-lg-7 col-md-6" style="margin-bottom:50px">
                         <div class="about_content">
-                            <h2>Selamat Datang Di AP2SC</h2>
+                            <h2 >Selamat Datang Di AP2SC</h2>
                             <h3>Aplikasi Pengembangan dan Pelatihan Service Center</h3>
-                            <p>  IT Training & Service Center (ITSC) UNIKOM merupakan salah satu produk unggulan dari IPTEK bagi inovasi dan kreatifitas kampus UNIKOM, yang berdiri sejak tahun 2008 sampai saat ini. ITSC UNIKOM merupakan lembaga yang bergerak di di bidang jasa pelayanan training/kursus bidang teknologi informasi dan multimedia, dan juga merupakan penyedia layanan jasa service center seperti smartphone dan laptop.
-
-                                    Selain itu, ITSC juga merupakan penyedia jasa untuk pembuatan Aplikasi dan Multimedia. Trainer yang berada di ITSC juga merupakan Trainer yang berpengalaman serta memiliki sertifikat kompetensi.</p>
-                            <p> </p>
+                            <p>Direktorat Aplikasi Pengembangan Pelatihan & Service Center (AP2SC) merupakan salah satu direktorat yang ada di Universitas Komputer Indonesia dan didirikan pada bulan November 2016. 
+                            Direktorat AP2SC dipimpin oleh Bobi Kurniawan, S.T., M.Kom. sejak 2016. Direktorat AP2SC memiliki tugas sebagai berikut :
+                            <ol type="1">
+                                <li>Koordinasi dan kerjasama dengan bagian lain dalam mempersiapkan pelatihan, workshop dan seminar nasional maupun internasional.
+                                <li>Melaksanakan kegiatan training dan service center dibidang teknologi informasi dan humaniora untuk lingkungan UNIKOM maupun di luar UNIKOM
+                                <li> Bertanggung jawab terhadap pelaksanaan mata kuliah Hardware Komputer
+                                <li> Bertanggung jawab terhadap pengelolaan sistem parkir di lingkungan UNIKOM
+                                <li> Bertanggung jawab terhadap operasional absensi UNIKOM dengan sistem Finger Print
+                                <li> Bertanggung jawab terhadap proses pendataan mahasiswa baru UNIKOM
+                                </li>
+                            </ol>     
                             <div class="row">
                               <div class="col">
                               </div>
